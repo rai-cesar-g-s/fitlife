91c9944179ed2f1fa6058d27360a916b674c85e7
Rai Cesar Gomes da Silva <rai.silva@DIREITOFRANCA.LOCAL>
2025-12-17 13:27:23 -0300

    feat: adicionar logica ao botão back-to-top

--- js/script.js
+++ js/script.js
@@ -15,3 +15,19 @@ const closeNavbar = () => navbar.classList.remove("active");
 addEventOnElem(navOpenBtns, "click", openNavbar);
 addEventOnElem(navCloseBtns, "click", closeNavbar);
 addEventOnElem(navLinks, "click", closeNavbar);
+
+// header & back to top btn active
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function (){
+  if (window.scrollY >= 100){
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  }
+  else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
